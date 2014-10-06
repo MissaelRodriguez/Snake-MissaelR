84238a5fbce4df3b2037a372711482798c4fb3ee
MissaelRodriguez <Blackphantom@gmail.com>
2014-10-06 14:19:19 -0700

    no message

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,11 +1,15 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
 
 gameInitialize();
+snakeInitialize();
 gameDraw();
+snakeDraw();
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -25,4 +29,29 @@ function gameLoop() {
 function gameDraw() {
     context.fillStyle = "rgb(180, 250, 213)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLenght = 5;
+    snakeSize = 50;
+    
+    for(var index = 0; index < snakeLenght; index++) {
+       snake.puch( {
+           x: index,
+           y: 0
+       }); 
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.lenght; index++){
+        contex.fillstyle = 'white';
+        conrex.fillrect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+    
+}
+
+function snakeUpdate() {
+    
 }
